f74fb8eb404995881b2b010800222fa56d11e08f
Pedro Lopes <pedro.lopes20@fatec.sp.gov.br>
2023-09-21 16:15:11 -0300

    ap07(POO-p2): comer  e dormir

--- Personagem.java
+++ Personagem.java
@@ -19,12 +19,27 @@ public class Personagem {
     }
 
     void comer() {
-        System.out.println(nome + " Comendo...");
+        if (fome>= 1){
+            System.out.println(nome + " Comendo...");
+            // operador ternário
+            energia = energia+1 >10 ? 10 : energia +1 ;
+            fome--;
+        }
+        else{
+            System.out.println(nome + " sem fome...");
+        }
     }
 
     void dormir() {
+        if (sono > 0){
         System.out.println(nome + " dormindo...");
+        sono -=1;
+        energia = Math.min(10, energia + 1);
     }
+    else{
+        System.out.println(nome + " sem sono...");
+    }
+}
 
     public static void main(String[] args) {
 
