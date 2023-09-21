64a1f866db2a0ea1a4aeea1a6f4a278c4bb3a1f4
Pedro Lopes <pedro.lopes20@fatec.sp.gov.br>
2023-09-21 17:30:44 -0300

    ap07(poo-p2): cacador aleatorio

--- Jogo.java
+++ Jogo.java
@@ -1,12 +1,29 @@
+import java.util.Random;
+
 public class Jogo {
-    
+
     public static void main(String[] args) throws Exception{
+        var gerador = new Random();
         var personagem = new Personagem();
         personagem.nome = "jonh";
+        var soneca = new Personagem();
+        //nasce com muito e sem energia. fome mais ou menos
+        //ele tem a probabilidade 80% de dormir
+        //10% de caçar
+        //10% de comer
         while(true){
-            personagem.cacar();
-            personagem.comer();
-            personagem.dormir();
+            int oQuefazer = gerador.nextInt(3); //[0..3]
+            switch(oQuefazer){
+                case 0: 
+                personagem.cacar();
+                break;
+                case 1: 
+                personagem.comer();
+                break;
+                case 2:
+                personagem.dormir();
+                break;
+            }
             System.out.printf(
             "e: %d, f: %d, s: %d\n", 
             personagem.energia,
@@ -14,7 +31,7 @@ public class Jogo {
             personagem.sono
             );
             System.out.println("************");
-        Thread.sleep(8000);
+        Thread.sleep(6000);
         }
     }
 }
